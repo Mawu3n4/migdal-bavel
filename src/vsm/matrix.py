30c4a944f624a032b9350a87a7b2e7d8e6976a5c
Zack-- <dibe.zack@gmail.com>
2015-05-06 10:15:22 +0100

    fix: some stuff, nothing to see here. ehem.

--- src/vsm/matrix.py
+++ src/vsm/matrix.py
@@ -6,22 +6,31 @@ class Matrix(list):
         if vectors:
             self.vectors = Vector([Vector(vector) for vector in vectors])
         elif x is not None and y is not None:
-            self.vectors = Vector([Vector([0]*x)]*y)
+            self.vectors = Vector([Vector([0]*x) for i in range(y)])
         else:
             self.vectors = Vector([Vector([0])])
         super().__init__(self.vectors)
+
+    def lencol(self):
+        return len(self[0])
+
+    def lenrow(self):
+        return len(self)
 
     def __mul__(self, operand):
         type_ = type(operand).__name__
 
         if type_ == 'Matrix':
-            if len(operand) != len(self):
+            if operand.lenrow() != self.lencol():
                 print("Can not multiply matrixs of different lengths")
                 return self
-            for y, vector in enumerate(operand):
-                for x, coordinate in enumerate(vector):
-                    self[y][x] *= coordinate
-            return self
+
+            res = Matrix(x=operand.lencol(), y=self.lenrow())
+            for y in range(self.lenrow()):
+                for x in range(operand.lencol()):
+                    res[y][x] = sum(self[y][i] * operand[i][x] for i in range(operand.lenrow()))
+
+            return res
 
     def normalize(self, norm):
         return Matrix(vectors=[vec.normalize(norm) for vec in self.vectors])
